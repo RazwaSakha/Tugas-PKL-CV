7af2f4ea008f0fd3d19ab94e53414c4ca6449915
RazwaSakha <razpradipa31@gmail.com>
2021-03-04 16:48:33 +0700

    first commit

---
+++ resources/views/CurriculumVitae.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>CURRICULUM VITAE</title>
+    </head>
+    <body>
+    <h1 align=center>CURRICULUM VITAE</h1>
+    <!--INFORMASI DATA DIRI-->
+        <hr>
+        <b>Informasi Pribadi</b>
+        <hr>
+        <table style="width:50%">
+            <!--FOTO-->
+            <img src="storage/fotoku.jpg" align="right" width="104" height="142">
+        <tr>
+        <th></th>
+        <th></th>
+        </tr>
+        <tr>
+            <td>Nama Lengkap</td>
+            <td>: Razwa Sakha Pradipa </td>
+        </tr>
+        <tr>
+            <td>Tempat Tanggal Lahir</td>
+            <td>: Sukabumi, 03 Januari 2004 </td>  
+        </tr>
+            <td>Alamat</td>
+            <td>: Perumahan Genting puri, Kec.Baros, Kota Sukabumi</td>
+        <tr>
+            <td>Status</td>
+            <td>: Pelajar</td>
+        </tr>
+            <tr>
+            <td>NO HP</td>
+            <td>: 083805421781</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>: razpradipa31@gmail.com</td>
+        </tr>
+        </table>
+    <!--RIWAYAT PENDIDIKAN FORMAL-->  
+            <hr>
+            <b>PENDIDIKAN FORMAL</b>
+            <hr>
+                <table style="width:80%">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <td>2019-2023</td>
+                <td>SMK Negeri 2 Sukabumi </td>
+            </tr>
+            <tr>
+                <td>2016-2019</td>
+                <td>SMPT Darul Amal </td>  
+            </tr>
+                <td>2010-2016</td>
+                <td>SDN Cipanas</td>
+            <tr>
+                <td>2008-2010</td>
+                <td>TK Pembina Lembursitu</td>
+            </tr>
+            </table>
+    <!-- RIWAYAT ORGANISASI-->
+                            <hr>
+                            <b>ORGANISASI</b><hr>
+                            <table style="width:180%">
+                        <tr>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                        <tr>
+                            <td>2020</td>
+                            <td>Paskibra</td>
+                        </tr>
+                        </table>
+    <!--KEAHLIAN YANG DIMILIKI-->                  
+                            <hr>
+                            <b>KEAHLIAN</b><hr>
+                        Merakit Komputer<br>
+                        Desain Grafis: CorelDraw<br>
+                        Video Editting: Adobe Premiere <br>
+    <a href="indeks.html">KEMBALI KE INDEKS</a><br>
+    </body>
+</html>
